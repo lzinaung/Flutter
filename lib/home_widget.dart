74473d704ac99677788a48daf0f6edf1b9fc8d56
lzinaung <60607293+lzinaung@users.noreply.github.com>
2020-02-04 18:08:04 +0630

    Add files via upload

---
+++ lib/home_widget.dart
@@ -0,0 +1,52 @@
+import 'package:f_fffff/home_view.dart';
+import 'package:f_fffff/pages.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomePage(),
+    ExplorePage(),
+    PastTripsPage()
+  ]; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Travel Budget App"),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text("Home"),
+          ),
+           BottomNavigationBarItem(
+            icon: new Icon(Icons.explore),
+            title: new Text("Explore"),
+          ),
+           BottomNavigationBarItem(
+            icon: new Icon(Icons.history),
+            title: new Text("Past Trips"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onTabTapped(int index){
+    setState((){
+      _currentIndex = index;
+    });
+  }
+}
